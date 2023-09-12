51e7daa062f1757aed6f315c263f4d08a58accc3
jacob06b <113119879+jacob06b@users.noreply.github.com>
2023-09-12 10:49:42 +0100

    Add files via upload

---
+++ Aoc2020-6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Aoc // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(twentysix());
+
+
+            int twentysix()
+            {
+                int counter = 0;
+                using (StreamReader sr = new StreamReader("PuzzleInput.txt"))
+                {
+                    HashSet<char> group = new HashSet<char>();
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        
+
+                        foreach (char i in line)
+                        {
+                            group.Add(i);
+                        }
+                        if (line == "")
+                        {
+                            
+                            counter += group.Count();
+                            group.Clear();
+
+                        }
+                    }
+                    return counter;
+
+                }
+
+
+            }
+
+        }
+
+        
+    }
+    
+    
+        
+    
+}
